88c903f43728579d69edab25153a9355bfd35543
vickyruud <aprajit.k@gmail.com>
2022-03-01 14:04:50 -0500

    move app to components and save bg image

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+  <div>
+  <h1>Ja<span class="highlight">mmm</span>ing</h1>
+  <div class="App">
+    <div class="App-playlist">
+    </div>
+  </div>
+</div>
+  );
+}
+
+export default App;
