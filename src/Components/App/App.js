abbf4dabc82892a652d7792a72ba1f16f6c24394
vickyruud <aprajit.k@gmail.com>
2022-03-01 16:00:02 -0500

    create searchbar, searchresults components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,3 +1,5 @@
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
 import './App.css';
 
 function App() {
@@ -5,11 +7,11 @@ function App() {
 <div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
-    {/* <!-- Add a SearchBar component --> */}
-    <div className="App-playlist">
-      {/* <!-- Add a SearchResults component --> */}
-      {/* <!-- Add a Playlist component --> */}
-    </div>
+    <SearchBar />
+        <div className="App-playlist">
+          <SearchResults />
+          {/* <!-- Add a Playlist component --> */}
+        </div>
   </div>
 </div>
   );
