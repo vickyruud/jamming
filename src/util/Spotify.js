9fd27e14ebaf1b7bed1e68f6a8db32bcb1abacdc
vickyruud <aprajit.k@gmail.com>
2022-03-03 16:54:37 -0500

    deployed to surge

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,7 +1,8 @@
 
 const clientId = '47f8572a626845fdbe9ba575cf8873c4'
-const redirectUri = "http://localhost:3000/";
-
+const redirectUri = "http://jamming-vickyruud.surge.sh";
+// const redirectUri = "http://localhost:3000/";
+ 
 let accessToken = '';
 
 const Spotify = {
