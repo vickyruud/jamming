dce86edbd2b96b5cf143159269f7d8ee81534f59
vickyruud <aprajit.k@gmail.com>
2022-03-03 17:00:25 -0500

    update comments to spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -52,7 +52,7 @@ const Spotify = {
         })
   },
 
-  //saves playlist to spotify
+  //saves playlist to spotify after obtaining user id
   savePlaylist(playlistName, trackURIs) {
     if (playlistName && trackURIs.length) {
       const accessToken = Spotify.getAccessToken();
